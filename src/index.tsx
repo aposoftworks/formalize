cec10c4f62a0c3509fc4762b71e7f3e71a40b1c0
Rafael <rafael.chaves98@hotmail.com>
2020-06-08 03:33:35 -0300

    Reverted package migration overwrite

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,40 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+//Modules components
+import _Form			from "./modules/Form";
+import _Group			from "./modules/Group";
+import _Input			from "./modules/Text/Input";
+import _Text			from "./modules/Text/Text";
+import _Toggle			from "./modules/Generic/Toggle";
+import _Select			from "./modules/Generic/Select";
+import _Wrapper			from "./modules/Wrapper";
+import _Value			from "./modules/Value";
+import _When 			from "./modules/When";
+import _File			from "./modules/Generic/File";
 
-interface Props {
-  text: string
-}
+//Separated components
+export const Form 		= _Form;
+export const Group		= _Group;
+export const Input		= _Input;
+export const Text		= _Text;
+export const Toggle		= _Toggle;
+export const Select		= _Select;
+export const Wrapper	= _Wrapper;
+export const Value		= _Value;
+export const When		= _When;
+export const File 		= _File;
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+//Bundled
+const bundled = {
+	//Components
+	Form: 		_Form,
+	Group:		_Group,
+	Input:		_Input,
+	Text:		_Text,
+	Toggle:		_Toggle,
+	Select:		_Select,
+	Wrapper:	_Wrapper,
+	Value:		_Value,
+	When:		_When,
+	File: 		_File,
+};
+
+export default bundled;
