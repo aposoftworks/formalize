3fb4d675889ec6228114f46a42e8f48d800f920d
Rafael <rafael.chaves98@hotmail.com>
2020-07-17 23:17:28 -0300

    Added more tests

--- src/tests/base.test.js
+++ src/tests/base.test.js
@@ -1,8 +1,15 @@
 // Components
-import Form 	from '../modules/Form';
-import Group 	from '../modules/Group';
-import Input 	from '../modules/Text/Input';
-import Text		from "../modules/Text/Text";
+import Form		from "./modules/Form";
+import Group	from "./modules/Group";
+import Input	from "./modules/Text/Input";
+import Text		from "./modules/Text/Text";
+import Toggle	from "./modules/Generic/Toggle";
+import Select	from "./modules/Generic/Select";
+import Wrapper	from "./modules/Wrapper";
+import Value	from "./modules/Value";
+import When 	from "./modules/When";
+import File		from "./modules/Generic/File";
+import Error	from "./modules/Error";
 
 describe('Components present', () => {
 	it('Form', () => {
@@ -19,5 +26,33 @@ describe('Components present', () => {
 
 	it('Text', () => {
 		expect(Text).toBeTruthy();
+	});
+
+	it('Toggle', () => {
+		expect(Toggle).toBeTruthy();
+	});
+
+	it('Select', () => {
+		expect(Select).toBeTruthy();
+	});
+
+	it('Wrapper', () => {
+		expect(Wrapper).toBeTruthy();
+	});
+
+	it('Value', () => {
+		expect(Value).toBeTruthy();
+	});
+
+	it('When', () => {
+		expect(When).toBeTruthy();
+	});
+
+	it('File', () => {
+		expect(File).toBeTruthy();
+	});
+
+	it('Error', () => {
+		expect(Error).toBeTruthy();
 	});
 });
